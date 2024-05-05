55e11b98f0f391d51457e36652d225e434890b49
LevRozengart <150509658+LevRozengart@users.noreply.github.com>
2024-05-05 12:38:00 +0500

    Create 1.py

---
+++ tasks2/1.py
@@ -0,0 +1,9 @@
+class book:
+    nazvanie = None
+    avtor = None
+    def get_nazvanie(self, nazvanie):
+        self.nazvanie = nazvanie
+
+    def get_avtor(self, avtor):
+        self.avtor = avtor
+
